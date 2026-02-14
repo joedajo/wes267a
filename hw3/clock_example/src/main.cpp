298da14109293a29d0ca342d23a3dee2a3350f77
Joseph Fields <joseph.g.fields2.civ@us.navy.mil>
2026-02-13 19:04:47 -0800

    hw3

---
+++ hw3/clock_example/src/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Alireza on 2/14/20.
+//  Copyright Â© 2020 Alireza. All rights reserved.
+//
+
+#include "main.h"
+#include "cycletime.h"
+#include "timer.h"
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	float cpu_timer;
+	unsigned int delay = 1;
+	
+	cout << "WES237A lab 4" << endl;
+
+	char key=0;
+	
+	// 1 argument on command line: delay = arg
+	if(argc >= 2)
+	{
+		delay = atoi(argv[1]);
+	}
+
+    //TODO: declare 2 cpu_count variables: 1 for before sleeping, 1 for after sleeping (see cpu_timer)
+    //TODO: initialize the counter
+
+    //TODO: get the cyclecount before sleeping
+    usleep(delay);
+    //TODO: get the cyclecount after sleeping
+
+    //TODO: subtract the before and after cyclecount
+    //TODO: print the cycle count (see the print statement for the cpu_timer below)
+
+	LinuxTimer t;
+	usleep(delay);
+	t.stop();
+	cpu_timer = t.getElapsed();
+
+	
+	cout << "Timer: " << (double)cpu_timer/1000000000.0 << endl;
+
+	return 0;
+}
+
